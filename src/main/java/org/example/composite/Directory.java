a7daa356ae381e6226b9d7b9f07d9d09a8ab78c5
takattty <r9r_celtics_t9t@icloud.com>
2024-08-19 22:06:07 +0900

    compositeパターン - 写経

---
+++ src/main/java/org/example/composite/Directory.java
@@ -0,0 +1,39 @@
+package org.example.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directory extends Entry {
+    private String name;
+    private List<Entry> directory = new ArrayList<>();
+
+    public Directory(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getSize() {
+        return directory.stream().mapToInt(Entry::getSize).sum();
+    }
+
+    @Override
+    protected void printList(String prefix) {
+        System.out.println(prefix + "/" + this);
+        for (Entry entry : directory) {
+            entry.printList(prefix + "/" + name);
+        }
+    }
+
+    // ディレクトリもファイルも入れられるのがポイント
+    // いちいちFile fileやDirectory dirとかしない。
+    // Entryで抽象化してるので、これで扱える。
+    public Entry add(Entry entry) {
+        directory.add(entry);
+        return this;
+    }
+}
---
+++ src/main/java/org/example/composite/Entry.java
@@ -0,0 +1,17 @@
+package org.example.composite;
+
+public abstract class Entry {
+    public abstract String getName();
+    public abstract int getSize();
+
+    public void printList() {
+        printList("");
+    }
+
+    protected abstract void printList(String prefix);
+
+    @Override
+    public String toString() {
+        return getName() + " (" + getSize() + ") ";
+    }
+}
