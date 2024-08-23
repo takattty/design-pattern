07ee6924f85b40a146c10b24f8c8fdcb3ef21b57
takattty <r9r_celtics_t9t@icloud.com>
2024-08-23 21:57:51 +0900

    stateパターン - enum導入の確認

--- src/main/java/org/example/state/StateUtil.java
+++ src/main/java/org/example/state/StateUtil.java
@@ -1,6 +1,10 @@
 package org.example.state;
 
 public enum StateUtil {
+    // 状態遷移用の処理を作るとなると、状態を知らないといけないことに気が付いた
+    // それって状態クラスと何が違うのか
+    // 自分の状態については知ってるけど、他の状態クラスを知らずに済むがメリットだろうな
+    // ただenumで持つ必要はあるんだろうか
     DAY(DayState.getInstance()),
     NIGHT(NightState.getInstance());
 
