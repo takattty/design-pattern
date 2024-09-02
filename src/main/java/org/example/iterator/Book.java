c719db35ee6b0fd00dee86f8ef349b6cd90b00ab
takattty <r9r_celtics_t9t@icloud.com>
2024-09-02 23:51:33 +0900

    Iteratorパターン：写経

---
+++ src/main/java/org/example/iterator/Book.java
@@ -0,0 +1,13 @@
+package org.example.iterator;
+
+public class Book {
+    private final String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
